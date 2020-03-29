c1ec5c6139913b307bcbea28aa480e7f255884a4
Artur Perwenis <aperwenis@gmail.com>
2020-03-29 17:20:01 +0200

    Working version V1.0

--- OneDrivePhotoRenamer/Program.cs
+++ OneDrivePhotoRenamer/Program.cs
@@ -16,37 +16,20 @@ namespace OneDrivePhotoRenamer
         //20190414_104305.mp4 => 2019-04-14 10.43.05.mp4
         static void Main(string[] args)
         {
-            #region Test
-            {
-                string inputName = "20190420_114155.jpg";
-                Regex rPattern = new Regex(@"(?'year'^\d{4})(?'month'\d{2})(?'day'\d{2})_(?'hh'\d{2})(?'mm'\d{2})(?'ss'\d{2})");
-                var match = rPattern.Match(inputName);
-                string newName = string.Format("{0}-{1}-{2} {3}.{4}.{5}",
-                    match.Groups["year"],
-                    match.Groups["month"],
-                    match.Groups["day"],
-                    match.Groups["hh"],
-                    match.Groups["mm"],
-                    match.Groups["ss"]);
-            }
-            #endregion
 
-
-            if (args.Length < 2)
+            if (args.Length < 1)
             {
                 Console.WriteLine("Too few arguments.");
                 Console.WriteLine("Recommmended syntax:");
-                Console.WriteLine("PhotoFileRenamer <dir> <NewNameFormat> [<SearchPattern>]");
+                Console.WriteLine("PhotoFileRenamer <dir> [<SearchPattern>]");
                 return;
             }
             string dir = args[0]; //Directory to process
-            string nameCore = args[1]; //How would you like your files to be named
-            //Number part length will be computed automatically based on the number of pictures being renamed.
             string pattern = "*.jpg"; //Default pattern
             //Handle optional pattern
-            if (args.Length > 2)
+            if (args.Length > 1)
             {
-                pattern = args[2];
+                pattern = args[1];
             }
             Console.WriteLine("Processing {0} files in {1} directory", pattern, dir);
             if (!Directory.Exists(dir))
@@ -58,18 +41,48 @@ namespace OneDrivePhotoRenamer
             FileInfo[] files = di.GetFiles(pattern, SearchOption.TopDirectoryOnly);
             Console.WriteLine("Found {0} files.", files.Length);
 
-            int numPart = files.Length.ToString().Length;
-            Console.WriteLine("Num part will take {0} characters.", numPart);
-
             //Dictionary of <original name>, <new name>
             Dictionary<string, string> renames = new Dictionary<string, string>();
             int pictureIndex = 1; //Index names from 1
-            //Need to escape it in a funny way
-            string nameFormat = string.Format("{0} {2}{1}{3}{4}", nameCore, numPart, "{0:D", "}", "{1}");
-            Console.WriteLine("Files will be renamed using this pattern: {0}", nameFormat);
+            #region Test
+            string inputName = "20190420_114155.jpg";
+            Regex rPattern = new Regex(@"(?'year'^\d{4})(?'month'\d{2})(?'day'\d{2})_(?'hh'\d{2})(?'mm'\d{2})(?'ss'\d{2})\s*(?'remainder'.*\Z)");
+            var match = rPattern.Match(inputName);
+            string newName = string.Format("{0}-{1}-{2} {3}.{4}.{5}",
+                match.Groups["year"],
+                match.Groups["month"],
+                match.Groups["day"],
+                match.Groups["hh"],
+                match.Groups["mm"],
+                match.Groups["ss"]);
+            #endregion
+
             foreach (FileInfo file in files)
             {
-                string newName = string.Format(nameFormat, pictureIndex, file.Extension.ToLower());
+                var nameNoExtension = new string(file.Name.Take(file.Name.Length - file.Extension.Length).ToArray());
+                match = rPattern.Match(nameNoExtension);
+                //newName = string.Format(nameFormat, pictureIndex, file.Extension.ToLower());
+                if (match.Groups["year"].Length != 4)
+                {
+                    Console.WriteLine("Skipping {0} because it does not follow OneDrive photo name pattern.", file.Name);
+                    continue;
+                }
+                newName = string.Format("{0}-{1}-{2} {3}.{4}.{5}",
+                        match.Groups["year"],
+                        match.Groups["month"],
+                        match.Groups["day"],
+                        match.Groups["hh"],
+                        match.Groups["mm"],
+                        match.Groups["ss"]);
+                //Handle remainder, if one exists.
+                var remainder = match.Groups["remainder"];
+                if (remainder.Length > 0)
+                {
+                    newName = string.Format("{0} {1}", newName, remainder);
+                }
+                //Add extension
+                newName = string.Format("{0}{1}", newName, file.Extension.ToLower());
+
                 string newFullName = string.Format("{0}\\{1}", file.DirectoryName, newName);
                 renames.Add(file.FullName, newFullName);
                 Console.WriteLine("{0} ==> {1}", file.Name, newName);
@@ -89,7 +102,7 @@ namespace OneDrivePhotoRenamer
             XmlDocument renameLog = new XmlDocument();
             XmlDeclaration declaration = renameLog.CreateXmlDeclaration("1.0", Encoding.UTF8.WebName, "yes");
             renameLog.AppendChild(declaration);
-            XmlNode rootXmlNode = renameLog.CreateNode(XmlNodeType.Element, "PhotoFileRenamer", myNamespace);
+            XmlNode rootXmlNode = renameLog.CreateNode(XmlNodeType.Element, "OneDrivePhotoRenamer", myNamespace);
             renameLog.AppendChild(rootXmlNode);
             XmlNode renamesNode = renameLog.CreateNode(XmlNodeType.Element, "renames", myNamespace);
             rootXmlNode.AppendChild(renamesNode);
@@ -112,7 +125,7 @@ namespace OneDrivePhotoRenamer
                     Console.WriteLine("Name lookup error! Should never happen.");
                 }
             }
-            string renameLogFile = Path.Combine(dir, "PhotoFileRenamer.xml");
+            string renameLogFile = Path.Combine(dir, "OneDrivePhotoRenamer.xml");
             renameLog.Save(renameLogFile);
         }
     }
